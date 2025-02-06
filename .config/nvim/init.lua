16546a62c564c3dace071705ddbee416bc9e660b
adbhitman <adbhitman@users.noreply.github.com>
2025-02-06 18:00:57 +0200

    WIP: transition to init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,65 +1,69 @@
+--
+-- COMMON
+--
+-- {{{
+-- Common settings {{{
+vim.o.compatible=false
+vim.o.number=true
+vim.o.relativenumber=true
+vim.o.hlsearch=true
+vim.o.incsearch=true
+vim.o.ignorecase=true
+vim.o.smartcase=true
+vim.o.encoding='utf-8'
+--vim.o.fileformat=unix
+--vim.o.fileencoding=utf-8
+
+vim.g.mapleader=''
+vim.g.maplocalleader=''
+
+vim.o.autoread=true
+vim.o.splitright=true
+
 vim.cmd([[
-"
-" COMMON
-"
-" {{{
-" Common settings {{{
-set nocompatible
-set number
-set relativenumber
-set hlsearch
-set incsearch
-set ignorecase
-set smartcase
-set encoding=utf-8
-"set fileformat=unix
-"set fileencoding=utf-8
-
-let mapleader=''
-let maplocalleader=''
-
-set autoread
-set splitright
-
 syntax on
 filetype plugin indent on
+]])
 
-" sets tabs to spaces
-set tabstop=4     " Size of TAB as spaces
-set softtabstop=4 " Sets the number of columns for a TAB
-set shiftwidth=4  " Indents will have a width of 4
-set expandtab     " Expand TABs to spaces
-set autoindent    " Copy indent from current line when starting a new line.
-set smartindent
+-- sets tabs to spaces
+vim.o.tabstop=4     -- Size of TAB as spaces=true
+vim.o.softtabstop=4 -- Sets the number of columns for a TAB=true
+vim.o.shiftwidth=4  -- Indents will have a width of 4=true
+vim.o.expandtab=true     -- Expand TABs to spaces=true
+vim.o.autoindent=true    -- Copy indent from current line when starting a new line.=true
+vim.o.smartindent=true
 
-set backspace=indent,eol,start
-set nojoinspaces
+vim.opt.backspace={'indent','eol','start'}
+vim.o.joinspaces=false
 
+vim.cmd([[
 setlocal omnifunc=syntaxcomplete#Complete
+]])
 
-set completeopt=menuone,preview,popup,fuzzy
+vim.opt.completeopt={'menuone','preview','popup','fuzzy'}
 
-set history=1000
-set messagesopt=hit-enter,history:1000
-"set autochdir
+vim.o.history=1000
+vim.opt.messagesopt={'hit-enter','history:1000'}
+--vim.o.autochdir=true
 
-set wildmenu
-set wildmode=longest,list
-set wildignore=*.docx,*.jpg,*.png,*.gif,*.pdf,*.pyc,*.exe,*.flv,*.img,*.xlsx
+vim.o.wildmenu=true
+vim.opt.wildmode={'longest','list'}
+vim.opt.wildignore={'*.docx','*.jpg','*.png','*.gif','*.pdf','*.pyc','*.exe','*.flv','*.img','*.xlsx'}
 
-set hidden
-set showcmd
-"set title
-set laststatus=2
+vim.o.hidden=true
+vim.o.showcmd=true
+--vim.o.title=true
+vim.o.laststatus=2
 
-set timeout
-set ttimeoutlen=100
-"set ruler
-set scrolloff=5
+vim.o.timeout=true
+vim.o.ttimeoutlen=100
+--vim.o.ruler=true
+vim.o.scrolloff=5
 
-set cursorline
-"set cursorcolumn
+vim.o.cursorline=true
+--set cursorcolumn
 
+vim.cmd([[
 augroup setcolorcolumn
     autocmd!
     autocmd BufEnter * if !&textwidth | setlocal textwidth=80
@@ -67,11 +71,13 @@ augroup setcolorcolumn
 augroup END
 
 highlight ColorColumn ctermbg=0 guibg=lightgrey
+]])
 
-"set spell
-"set spelllang=en
-" }}}
+vim.o.spell=true
+--vim.o.spelllang='en'
+-- }}}
 
+vim.cmd([[
 " Mappings {{{
 nnoremap <F1> :nohlsearch<CR>
 inoremap jk <Esc>
@@ -114,8 +120,10 @@ function! ShowCodeOutput(compiler)
 endfunction
 " }}}
 " }}}
+]])
 
 
+vim.cmd([[
 "
 " PLUGINS
 "
