cdd82274e1a651b250c240c85407399ef986d72a
adbhitman <adbhitman@users.noreply.github.com>
2025-02-12 19:59:52 +0200

    Add plugins
    - Added mason and mason-lspconfig as placeholder for now
    - Also some minor tweaks

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -248,8 +248,37 @@ require("lazy").setup({
          -- }}}
         },
         {
+         -- mason {{{
+         "williamboman/mason.nvim",
+         config=function()
+             require("mason").setup()
+         end
+         -- }}}
+        },
+        {
+         -- mason-lspconfig.nvim {{{
+         'williamboman/mason-lspconfig.nvim',
+         config=function()
+             require("mason-lspconfig").setup()
+             -- require("mason-lspconfig").setup_handlers {
+                 --     -- The first entry (without a key) will be the default handler
+                 --     -- and will be called for each installed server that doesn't have
+                 --     -- a dedicated handler.
+                 --     function (server_name) -- default handler (optional)
+                 --         require("lspconfig")[server_name].setup {}
+                 --     end,
+                 --     -- Next, you can provide a dedicated handler for specific servers.
+                 --     -- For example, a handler override for the `rust_analyzer`:
+                 --     ["rust_analyzer"] = function ()
+                 --         require("rust-tools").setup {}
+                 --     end
+                 -- }
+               end
+         -- }}}
+        },
+        {
          -- nvim-lspconfig {{{
-            'neovim/nvim-lspconfig',
+         'neovim/nvim-lspconfig',
          -- }}}
         },
         {
@@ -462,7 +491,10 @@ require("lazy").setup({
 -- }}}
 
 
--- My own notes {{{
+--
+-- My own notes
+--
+-- {{{
 -- Some LSP servers needed:
 --   Install manually:
 --   - https://github.com/latex-lsp/texlab
