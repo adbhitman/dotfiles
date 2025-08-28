78ba2904701edab7ca9ebeb4b94f445036459a58
adbhitman <adbhitman@users.noreply.github.com>
2025-08-28 14:45:47 +0300

    Edit lua_ls settings
    Change settings from documentation corresponding my usage.

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -16,12 +16,39 @@ return {
       vim.lsp.config("jsonls", { capabilities = capabilities })
       vim.lsp.config("lua_ls", {
         capabilities = capabilities,
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" },
+        on_init = function(client)
+          if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if
+              path ~= vim.fn.stdpath("config")
+              and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+            then
+              return
+            end
+          end
+
+          client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+            runtime = {
+              -- Tell the language server which version of Lua you're using
+              -- (most likely LuaJIT in the case of Neovim)
+              version = "LuaJIT",
             },
-          },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME,
+                -- Depending on the usage, you might want to add additional paths here.
+                -- "${3rd}/luv/library"
+                -- "${3rd}/busted/library",
+              },
+              -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+              -- library = vim.api.nvim_get_runtime_file("", true)
+            },
+          })
+        end,
+        settings = {
+          Lua = {},
         },
       })
       vim.lsp.config("marksman", { capabilities = capabilities })
@@ -37,6 +64,17 @@ return {
         "lua_ls",
         "marksman",
         "texlab",
+      })
+
+      vim.lsp.inline_completion.enable(true)
+      vim.keymap.set("i", "<C-CR>", function()
+        if not vim.lsp.inline_completion.get() then
+          return "<C-CR>"
+        end
+      end, {
+        expr = true,
+        replace_keycodes = true,
+        desc = "Get the current inline completion",
       })
 
       vim.api.nvim_create_autocmd("LspAttach", {
