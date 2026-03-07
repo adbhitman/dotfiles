aefe95eb7bafab74e1f7d03c8552e03adbc51877
adbhitman <adbhitman@users.noreply.github.com>
2026-03-07 22:10:54 +0200

    Fix missing space

--- .config/nvim/plugin/toggle.lua
+++ .config/nvim/plugin/toggle.lua
@@ -17,7 +17,7 @@ function! QuickFixToggle()
     if g:quickfix_is_open
         cclose
         let g:quickfix_is_open=0
-        execute g:quickfix_return_to_window . "wincmd w"
+        execute g:quickfix_return_to_window . " wincmd w"
     else
         let g:quickfix_return_to_window=winnr()
         copen
