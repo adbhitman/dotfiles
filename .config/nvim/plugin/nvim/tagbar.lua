7c6e967b523683decf2f8d76e64af9c16f1c1ea3
adbhitman <adbhitman@users.noreply.github.com>
2026-04-23 15:39:07 +0300

    Cleanup
    - noremap is true by default

--- .config/nvim/plugin/nvim/tagbar.lua
+++ .config/nvim/plugin/nvim/tagbar.lua
@@ -3,4 +3,4 @@
 vim.pack.add({ "https://github.com/preservim/tagbar" })
 
 vim.g.tagbar_width = 40
-vim.keymap.set("n", "<Leader>tb", ":TagbarToggle<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>tb", ":TagbarToggle<CR>")
