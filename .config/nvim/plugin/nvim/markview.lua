4b42707d76f580b2b88a2edcc16cfe0a25839956
adbhitman <adbhitman@users.noreply.github.com>
2026-04-29 17:32:34 +0300

    Edit indent for mardown viewing
    - Change indent form 4 to closer 2 in viewing mode

--- .config/nvim/plugin/nvim/markview.lua
+++ .config/nvim/plugin/nvim/markview.lua
@@ -10,6 +10,21 @@ require("markview").setup({
     enable = false,
     icon_provider = "devicons", -- "mini" or "devicons"
   },
+
+  markdown = {
+    list_items = {
+      shift_width = function(buffer, item)
+        ---@type integer Parent list items indent. Must be at least 1.
+        local parent_indnet = math.max(1, item.indent - vim.bo[buffer].shiftwidth)
+        return item.indent * (1 / (parent_indnet * 2))
+      end,
+      marker_minus = {
+        add_padding = function(_, item)
+          return item.indent > 1
+        end,
+      },
+    },
+  },
 })
 
 vim.api.nvim_set_keymap(
