e722a60a84201f2f79d093a7bebae02b118b54d1
adbhitman <adbhitman@users.noreply.github.com>
2026-05-28 16:28:38 +0300

    Fix conflicting mapping with <Tab>
    - Fix mappings with ultisnips
    - Need to remember that ultisnips is not using require().setup()-API and
      behaves more like an old vim-plugin when sourcing settings
    - Define global ultisnip mappings before loading it
    - Solves conflict with my <Tab> navigation in completion list

--- .config/nvim/plugin/nvim/ultisnips.lua
+++ .config/nvim/plugin/nvim/ultisnips.lua
@@ -1,6 +1,12 @@
 -- ultisnips
 -- Allows to write own snippets to spesific languages or commonly all
-vim.pack.add({ "https://github.com/SirVer/ultisnips" })
+
+-- Define your global ultisnips shortcuts before loading to pluging to actually
+-- get those to work
+vim.g.UltiSnipsExpandOrJumpTrigger = "<c-j>"
 
 -- If you want :UltiSnipsEdit to split your window.
 -- vim.g.UltiSnipsEditSplit = "vertical"
+
+-- Load the plugin
+vim.pack.add({ "https://github.com/SirVer/ultisnips" })
