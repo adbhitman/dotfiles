bed6d69c5a5915dccd7cbc99bc0c9cfc16fa6113
adbhitman <adbhitman@users.noreply.github.com>
2026-09-01 19:56:44 +0300

    Restore Q-mapping since it is used by multicursor
    - Q is now used in marking multicursor points
    - See neovim git commit 9a29622b545fc76c8b44d0e2b90f318a599eef39

--- .config/nvim/lua/common/mappings.lua
+++ .config/nvim/lua/common/mappings.lua
@@ -1,6 +1,5 @@
 vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("v", "<C-c>", '"+y')
-vim.keymap.set("n", "Q", "<Nop>")
 
 vim.keymap.set({ "n" }, "<F8>", ":w | lua ShowOutput({compiler='python3'})<CR>")
 vim.keymap.set({ "i" }, "<F8>", "<Esc>:w | lua ShowCodeOutput({compiler='python3'})<CR>")
